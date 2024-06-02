6eb62bc5471ab755dccfc9d2d298bdf6d321ba96
lusia <panfluba@gmail.com>
2024-06-02 20:45:52 +0300

    feat: make active buttons into Pizza`s component and add spread operator instead title={value.title}...

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
             {pizzas.map((value) => (
-              <PizzaBlock title={value.title} price={value.price} image={value.imageUrl} />
+              <PizzaBlock {...value} />
             ))}
           </div>
         </div>
--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -1,25 +1,36 @@
 import React from 'react';
 
 //деструктуризация пропса (props), обращение {props.title}
-function PizzaBlock({ title, price, image }) {
+function PizzaBlock({ title, price, image, sizes, types }) {
   const [pizzaCount, setPizzaCount] = React.useState(0);
+  const [activeType, setActiveType] = React.useState(0);
+  const [activeSize, setActiveSize] = React.useState(0);
+
+  const typeNames = ['традиционное', 'тонкое'];
 
   const onClickAdd = () => {
     setPizzaCount(pizzaCount + 1);
   };
+
   return (
     <div className="pizza-block">
       <img className="pizza-block__image" src={image} alt="Pizza" />
       <h4 className="pizza-block__title">{title}</h4>
       <div className="pizza-block__selector">
         <ul>
-          <li className="active">тонкое</li>
-          <li>традиционное</li>
+          {types.map((type) => (
+            <li onClick={() => setActiveType(type)} className={activeType === type ? 'active' : ''}>
+              {typeNames[type]}
+            </li>
+            //type совпадает с id
+          ))}
         </ul>
         <ul>
-          <li className="active">26 см.</li>
-          <li>30 см.</li>
-          <li>40 см.</li>
+          {sizes.map((size, id) => (
+            <li onClick={() => setActiveSize(id)} className={activeSize === id ? 'active' : ''}>
+              {size} см.
+            </li>
+          ))}
         </ul>
       </div>
       <div className="pizza-block__bottom">
